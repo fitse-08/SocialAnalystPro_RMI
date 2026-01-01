6987f49a98645193d0c511aa7984f1bd9753e03a
HuzeThat <nesinasuha@gmail.com>
2026-01-01 12:35:20 +0300

    added helloapplication

---
+++ src/main/java/com/example/socialanalystpro/HelloApplication.java
@@ -0,0 +1,107 @@
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git init
+Reinitialized existing Git repository in C:/Users/hp/Music/Professional JavaFX Shop Billing System with MySQL/.git/
+
+Changes not staged for commit:
+        (use "git add/rm <file>..." to update what will be committed)
+        (use "git restore <file>..." to discard changes in working directory)
+deleted:    src/main/java/com/shopbilling/E-ShopBillingSystem
+deleted:    target/classes/com/shopbilling/E-ShopBillingSystem/README.md
+
+no changes added to commit (use "git add" and/or "git commit -a")
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git add .
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git commit -m "added the first files"
+        [main de1e86f] added the first files
+ 2 files changed, 2 deletions(-)
+delete mode 160000 src/main/java/com/shopbilling/E-ShopBillingSystem
+delete mode 100644 target/classes/com/shopbilling/E-ShopBillingSystem/README.md
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git remote remove origin
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git remote add origin https://github.com/HuzeThat/E-ShopBillingSystem.git
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git pull origin main --allow-unrelate
+d-histories --no-rebase
+remote: Enumerating objects: 3, done.
+        remote: Counting objects: 100% (3/3), done.
+        remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+Unpacking objects: 100% (3/3), 858 bytes | 25.00 KiB/s, done.
+From https://github.com/HuzeThat/E-ShopBillingSystem
+        * branch            main       -> FETCH_HEAD
+ * [new branch]      main       -> origin/main
+Auto-merging README.md
+CONFLICT (add/add): Merge conflict in README.md
+Automatic merge failed; fix conflicts and then commit the result.
+
+        C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git commit -m "merged remote README w
+ith localfiles"
+U       README.md
+error: Committing is not possible because you have unmerged files.
+        hint: Fix them up in the work tree, and then use 'git add/rm <file>'
+hint: as appropriate to mark resolution and make a commit.
+        fatal: Exiting because of an unresolved conflict.
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git push -u origin main
+To https://github.com/HuzeThat/E-ShopBillingSystem.git
+        ! [rejected]        main -> main (non-fast-forward)
+error: failed to push some refs to 'https://github.com/HuzeThat/E-ShopBillingSystem.git'
+hint: Updates were rejected because the tip of your current branch is behind
+hint: its remote counterpart. If you want to integrate the remote changes,
+hint: use 'git pull' before pushing again.
+        hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+        C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git init
+Reinitialized existing Git repository in C:/Users/hp/Music/Professional JavaFX Shop Billing System with MySQL/.git/
+
+Changes not staged for commit:
+        (use "git add/rm <file>..." to update what will be committed)
+        (use "git restore <file>..." to discard changes in working directory)
+deleted:    src/main/java/com/shopbilling/E-ShopBillingSystem
+deleted:    target/classes/com/shopbilling/E-ShopBillingSystem/README.md
+
+no changes added to commit (use "git add" and/or "git commit -a")
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git add .
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git commit -m "added the first files"
+        [main de1e86f] added the first files
+ 2 files changed, 2 deletions(-)
+delete mode 160000 src/main/java/com/shopbilling/E-ShopBillingSystem
+delete mode 100644 target/classes/com/shopbilling/E-ShopBillingSystem/README.md
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git remote remove origin
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git remote add origin https://github.com/HuzeThat/E-ShopBillingSystem.git
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git pull origin main --allow-unrelate
+d-histories --no-rebase
+remote: Enumerating objects: 3, done.
+        remote: Counting objects: 100% (3/3), done.
+        remote: Total 3 (delta 0), reused 0 (delta 0), pack-reused 0 (from 0)
+Unpacking objects: 100% (3/3), 858 bytes | 25.00 KiB/s, done.
+From https://github.com/HuzeThat/E-ShopBillingSystem
+        * branch            main       -> FETCH_HEAD
+ * [new branch]      main       -> origin/main
+Auto-merging README.md
+CONFLICT (add/add): Merge conflict in README.md
+Automatic merge failed; fix conflicts and then commit the result.
+
+        C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git commit -m "merged remote README w
+ith localfiles"
+U       README.md
+error: Committing is not possible because you have unmerged files.
+        hint: Fix them up in the work tree, and then use 'git add/rm <file>'
+hint: as appropriate to mark resolution and make a commit.
+        fatal: Exiting because of an unresolved conflict.
+
+C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>git push -u origin main
+To https://github.com/HuzeThat/E-ShopBillingSystem.git
+        ! [rejected]        main -> main (non-fast-forward)
+error: failed to push some refs to 'https://github.com/HuzeThat/E-ShopBillingSystem.git'
+hint: Updates were rejected because the tip of your current branch is behind
+hint: its remote counterpart. If you want to integrate the remote changes,
+hint: use 'git pull' before pushing again.
+        hint: See the 'Note about fast-forwards' in 'git push --help' for details.
+
+        C:\Users\hp\Music\Professional JavaFX Shop Billing System with MySQL>
